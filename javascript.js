07d0d1c860ba63826dfa22e9c9df8d31f0d8c677
Victor Pereira <victorhenriquep.ti@gmail.com>
2024-04-15 20:40:23 -0300

    Add js

---
+++ javascript.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const linkCachaca = document.getElementById('link-cachaca');
+    const modal = document.getElementById('modal-confirmacao-idade');
+
+    linkCachaca.addEventListener('click', function(event) {
+        event.preventDefault();
+        modal.style.display = 'block';
+    });
+
+    document.getElementById('btnSim').addEventListener('click', function() {
+        // O usuário confirmou que tem mais de 18 anos.
+        window.location.href = "cachaca.html";
+      });
+      
+      document.getElementById('btnNao').addEventListener('click', function() {
+        // O usuário confirmou que não tem mais de 18 anos.
+        window.location.href = "index.html";
+        modal.style.display = 'none';
+        alert("Desculpe, você deve ter pelo menos 18 anos para acessar esta página.");
+
+      });
+});
+
+
